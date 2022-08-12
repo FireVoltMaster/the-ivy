71af4bd78264589c2f79c82eef1e752f68ebe265
FireVoltMaster <mossernest291@gmail.com>
2022-08-12 11:45:00 -0400

    making the event template more programatic

--- pages/events/event-template.js
+++ pages/events/event-template.js
@@ -4,6 +4,52 @@ import Head from 'next/head'
 import Link from 'next/link'
 import Footer from '../../components/Footer'
 
+
+// image still hard coded: line 140
+
+const eventDeets = [
+  {
+    id: 1,
+    image: '/images/events/coming-soon.jpg',
+  },
+  {
+    id: 2,
+    date: '8.28.22',
+  },
+  {
+    id: 3,
+    title: 'Grand Opening',
+  },
+  {
+    id: 4,
+    heading: "The Grand Opening of The Ivy in Long Beach, NY."
+  },
+  {
+    id: 5,
+    subheading: "We can't wait to meet you!",
+  },
+  {
+    id: 6,
+    description: "We have been working hard on getting The Ivy ready and we are almost there! Follow us on Instagram for updates!",
+    // description: [
+    //   {
+    //     id: 1,
+    //     paragraph1: "We have been working hard on getting The Ivy ready and we are almost there!"
+    //   },
+
+    //   {
+    //     id: 2,
+    //     paragraph2: "Follow us on Instagram for updates!"
+    //   },
+
+    //   {
+    //     id: 3,
+    //     paragraph3: ""
+    //   },
+    // ],
+  },
+]
+
 export default function EventTemplate() {
   return (
     <div className="bg-white overflow-hidden">
@@ -22,28 +68,35 @@ export default function EventTemplate() {
       </div>
       <div className="pl-5">
         <Link href="/#calendar">
+          <a className="flex flex-row">
             <button
                 type="button"
                 className="z-50 px-3.5 mt-8 py-2 bg-gradient-to-b from-neon-pink via-neon-pink to-pink-400 text-white hover:bg-gradient-to-t from-neon-pink via-neon-pink to-pink-400 hover:text-white font-light rounded-md text-lg flex flex-row items-center"
-            >
-              <a className="flex flex-row">
-              <svg className="w-5 h-5 pt-2" aria-hidden="true" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
+            >  
+              <svg className="w-5 h-5" aria-hidden="true" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
                 <path d="M224 480c-8.188 0-16.38-3.125-22.62-9.375l-192-192c-12.5-12.5-12.5-32.75 0-45.25l192-192c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25L77.25 256l169.4 169.4c12.5 12.5 12.5 32.75 0 45.25C240.4 476.9 232.2 480 224 480z"/>
               </svg>
                 Back
-              </a>
             </button>
+          </a>
         </Link>
       </div>
+
       <div className="relative max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:px-8">
         <div className="hidden lg:block bg-gray-50 absolute top-0 bottom-0 left-3/4 w-screen" />
         <div className="mx-auto text-base max-w-prose lg:grid lg:grid-cols-2 lg:gap-8 lg:max-w-none">
           <div>
-            <h2 className="text-base text-neon-pink font-semibold tracking-wide uppercase">8.28.22</h2>
-            <h3 className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-              Grand Opening
-            </h3>
+            {eventDeets.map((deet) => (
+              <h2 key={deet.id} className="text-base text-neon-pink font-semibold tracking-wide uppercase">{deet.date}</h2>
+            ))}
+
+            {eventDeets.map((deet) => (
+              <h3 key={deet.id} className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+                {deet.title}
+              </h3>
+            ))}
           </div>
+          
         </div>
         <div className="mt-8 lg:grid lg:grid-cols-2 lg:gap-8">
           <div className="relative lg:row-start-1 lg:col-start-2">
@@ -71,16 +124,20 @@ export default function EventTemplate() {
             </svg>
             <div className="relative text-base mx-auto max-w-prose lg:max-w-none">
               <figure>
-                <div className="lg:aspect-none">
-                  {/* <img
-                    className="rounded-lg shadow-lg object-cover object-center"
-                    src="/images/events/sugarleaf.webp"
-                    alt="flyer"
-                    width={1184}
-                    height={1376}
-                  /> */}
+              {/* {eventDeets.map((deet) => (
+                <div key={deet.id} className="lg:aspect-none">
                     <Image 
-                      src="/images/events/event-flyer.jpg"
+                      src={deet.image}
+                      alt="Flyer"
+                      width={1184}
+                      height={1376}
+                      className="rounded-lg shadow-lg object-cover object-center"
+                    />
+                </div>
+                ))} */}
+                <div className="lg:aspect-none">
+                    <Image 
+                      src='/images/events/coming-soon.jpg'
                       alt="Flyer"
                       width={1184}
                       height={1376}
@@ -94,40 +151,29 @@ export default function EventTemplate() {
             </div>
           </div>
           <div className="mt-8 lg:mt-0">
-            <div className="text-base max-w-prose mx-auto lg:max-w-none">
+
+            {eventDeets.map((deet) => (
+            <div key={deet.id} className="text-base max-w-prose mx-auto lg:max-w-none">
               <p className="text-lg text-gray-500">
-                The Grand Opening of The Ivy in Long Beach, NY.
+                {deet.heading}
               </p>
             </div>
+            ))}
+            
             <div className="mt-5 prose prose-indigo text-gray-500 mx-auto lg:max-w-none lg:row-start-1 lg:col-start-1">
-              {/* <p>
-                Sollicitudin tristique eros erat odio sed vitae, consequat turpis elementum. Lorem nibh vel, eget
-                pretium arcu vitae. Eros eu viverra donec ut volutpat donec laoreet quam urna.
-              </p>
-              <p>
-                Bibendum eu nulla feugiat justo, elit adipiscing. Ut tristique sit nisi lorem pulvinar. Urna, laoreet
-                fusce nibh leo. Dictum et et et sit. Faucibus sed non gravida lectus dignissim imperdiet a.
-              </p>
-              <p>
-                Dictum magnis risus phasellus vitae quam morbi. Quis lorem lorem arcu, metus, egestas netus cursus. In.
-              </p> */}
-              {/* <ul role="list">
-                <li>Quis elit egestas venenatis mattis dignissim.</li>
-                <li>Cras cras lobortis vitae vivamus ultricies facilisis tempus.</li>
-                <li>Orci in sit morbi dignissim metus diam arcu pretium.</li>
-              </ul>
-              <p>
-                Rhoncus nisl, libero egestas diam fermentum dui. At quis tincidunt vel ultricies. Vulputate aliquet
-                velit faucibus semper. Pellentesque in venenatis vestibulum consectetur nibh id. In id ut tempus
-                egestas. Enim sit aliquam nec, a. Morbi enim fermentum lacus in. Viverra.
-              </p> */}
-              <h3>A subheading</h3>
-              <p>
-                About the event? Details?  More description. More description. More description. More description. More description. More description.
-              </p>
-              <p>
-                Anything else you want to say. More description. More description. More description. More description. More description. More description.
-              </p>
+
+              {eventDeets.map((deet) => (
+                <h3 key={deet.id}>{deet.subheading}</h3>
+              ))}
+
+              {eventDeets.map((deet) =>(
+              <div key={deet.id}>
+                <p> 
+                  {deet.description}
+                </p>              
+              </div>
+              ))}
+
             </div>
           </div>
         </div>
