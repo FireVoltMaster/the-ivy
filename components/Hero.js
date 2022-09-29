179885060b1519fec9998015e8740bc6574e8319
FireVoltMaster <mossernest291@gmail.com>
2022-09-29 15:15:32 -0400

    z index bug 🐛  fix

--- components/Hero.js
+++ components/Hero.js
@@ -46,7 +46,7 @@ export default function Hero() {
                     <a className="flex flex-row">
                       <button
                         type="button"
-                        className="z-50 px-3.5 mt-8 py-2 bg-gradient-to-b from-neon-pink via-neon-pink to-pink-400 text-white hover:bg-gradient-to-t from-neon-pink via-neon-pink to-pink-400 hover:text-white font-light rounded-md text-lg flex flex-row items-center"
+                        className="px-3.5 mt-8 py-2 bg-gradient-to-b from-neon-pink via-neon-pink to-pink-400 text-white hover:bg-gradient-to-t from-neon-pink via-neon-pink to-pink-400 hover:text-white font-light rounded-md text-lg flex flex-row items-center"
                       >  
                         SEE ALL EVENTS
                         <div className="pl-2">
