5a699e85dbb9de5cfca7115fce9806b38a584150
FireVoltMaster <mossernest291@gmail.com>
2023-05-12 15:11:56 -0400

    Update event-list.js
    Gift Voucher coupon added

--- pages/event-list.js
+++ pages/event-list.js
@@ -71,11 +71,23 @@ export default function EventList() {
                       <path strokeLinecap="round" strokeLinejoin="round" d="M16.5 6v.75m0 3v.75m0 3v.75m0 3V18m-9-5.25h5.25M7.5 15h3M3.375 5.25c-.621 0-1.125.504-1.125 1.125v3.026a2.999 2.999 0 010 5.198v3.026c0 .621.504 1.125 1.125 1.125h17.25c.621 0 1.125-.504 1.125-1.125v-3.026a2.999 2.999 0 010-5.198V6.375c0-.621-.504-1.125-1.125-1.125H3.375z" />
                   </svg>
                   <div className="pl-3">
-                    Tickets for May events
+                    Tickets for May & June events
                   </div>
                 </button>
               </a>
             </Link>
+            <div className="pt-5">
+              <Image
+                src="/images/events/eventvoucher.jpg"
+                className="rounded-xl"
+                alt="the ivy"
+                width={900}
+                height={400}
+                blurDataURL="/images/events/eventvoucher.jpg"
+                placeholder="blur"
+                loading="lazy"
+              />
+            </div>
           </div>
         </div>
         <div className="mt-8 lg:grid lg:grid-cols-2 lg:gap-8">
